82382378c54129aad9c31a7e857a506197fb097e
Tome Boshevski <tome@storj.io>
2023-09-26 15:40:02 +0200

    Fix outlines

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,10 @@ import clsx from 'clsx'
 
 const styles = {
   primary:
-    'rounded-lg bg-storj-blue-700 py-3 px-5 text-sm font-bold text-white hover:bg-storj-blue-600 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/50 active:bg-storj-blue-500',
+    'rounded-lg bg-storj-blue-700 py-3 px-5 text-sm font-bold text-white hover:bg-storj-blue-600 focus-visible:outline-offset-2 active:bg-storj-blue-500',
     
   secondary:
-    'rounded-lg bg-slate-800 py-3 px-5 text-sm font-bold text-white hover:bg-slate-700 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/50 active:text-slate-400',
+    'rounded-lg bg-slate-800 py-3 px-5 text-sm font-bold text-white hover:bg-slate-700 focus-visible:outline-2 focus-visible:outline-offset-2 active:text-slate-400',
 }
 
 export function Button({ variant = 'primary', className, href, ...props }) {
